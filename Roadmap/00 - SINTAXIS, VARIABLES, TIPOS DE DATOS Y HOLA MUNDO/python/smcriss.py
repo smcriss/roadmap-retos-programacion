2a092e779d9f37cecce10147e93ea24635bae8b8
smcriss <smcristobal07@gmail.com>
2026-09-19 22:39:57 +0200

    Fix variable values and add comments in smcriss.py
    Updated variable assignments and added multi-line comments.

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/smcriss.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/smcriss.py
@@ -1,1 +1,11 @@
-https//:
+https//:python.org
+"""
+Comentarios en 
+varias
+lineas
+"""
+
+my_variable = "verde"
+my_variable = "verde oscuro"
+MY_CONSTANTE = "mi constante"
+MY_CONSTANTE = "FERXOOOOO"
